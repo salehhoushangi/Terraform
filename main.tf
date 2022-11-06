836b292ac06d3b4c18ea430d4f824a4027ac2b47
saleh houshangi <saleh.houshangi@ionos.com>
2022-11-06 20:01:03 +0100

    add security group

--- main.tf
+++ main.tf
@@ -7,7 +7,36 @@ provider "aws" {
 
 ###Resource
 resource "aws_instance" "my_instance" {
-    ami = nonsensitive(data.aws_ssm_parameter.ami.value)
+    ami = "ami-0c55b159cbfafe1f0"
     instance_type = "t2.micro"
+    vpc_security_group_ids = [aws_security_group.instance.id]
+
+
+    user_data = <<-EOF
+    #!/bin/bash
+    echo ""Hello,world" > index.html
+    nohup busybox httpd -f -p 8080 &
+    EOF
+
+    tags = {
+      "Name" = "terraform-example"
+    }
   
 }
+resource "aws_security_group" "instance" {
+    name = "terraform-example-instance"
+
+    ingress =  {
+      cidr_blocks = [ "0.0.0.0/0" ]
+      from_port = 8080
+      protocol = "tcp"
+      to_port = 8080
+    } 
+
+  
+}
+
+########date
+#data "aws_ssm_parameter" "ami" {
+#  name = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
+#}

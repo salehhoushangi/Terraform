0af31f84bc18d25a60c152f23e75e411e3c7c1fa
saleh houshangi <saleh.houshangi@ionos.com>
2022-11-06 20:32:23 +0100

    add main.tf

---
+++ main.tf
@@ -0,0 +1,36 @@
+####provider####
+provider "aws" {
+    region = "us-east-2"
+  
+}
+
+###Resource
+resource "aws_instance" "my_instance" {
+    ami = "ami-0c55b159cbfafe1f0"
+    instance_type = "t2.micro"
+    vpc_security_group_ids = [aws_security_group.instance.id]
+
+
+    user_data = <<-EOF
+    #!/bin/bash
+    echo ""Hello,world" > index.html
+    nohup busybox httpd -f -p 8080 &
+    EOF
+
+    tags = {
+      "Name" = "terraform-example"
+    }
+  
+}
+resource "aws_security_group" "instance" {
+    name = "terraform-example-instance"
+
+    ingress {
+      cidr_blocks = [ "0.0.0.0/0" ]
+      from_port = 8080
+      protocol = "tcp"
+      to_port = 8080
+    } 
+
+  
+}
